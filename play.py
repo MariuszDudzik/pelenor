e57fc5f448966cea2ee9ad3fb3ea99c5682db584
MariuszDudzik <m.dudzik.067@studms.ug.edu.pl>
2025-02-21 17:02:07 +0100

    add to play(stateField, map)

--- play.py
+++ play.py
@@ -13,8 +13,15 @@ class Play(object):
         self.playerSfield = control_obj.Label(0, self.playerWfield.getHeight(), screenWidth * 0.1, 
             screenHeight // 2, kolor.RED, "", None, int(screenHeight * 0.035), kolor.WHITE, None, 
             None, None, None)
- 
+        self.stateField = control_obj.Label(screenWidth - screenWidth * 0.1, 0, screenWidth * 0.101, screenHeight,
+            kolor.BLUE, "", None, int(screenHeight * 0.035), kolor.WHITE, None, None, None, None)
+        self.map = control_obj.Label(screenWidth * 0.1, 0, screenWidth + screenWidth * 0.366, screenHeight +          screenHeight * 0.462, kolor.ORANGE, "", None, int(screenHeight * 0.035), kolor.WHITE, None, None, None, None)
+        
+
 
     def drawPlay(self, screen):
         self.playerWfield.draw(screen)
         self.playerSfield.draw(screen)
+        self.map.draw(screen)
+        self.stateField.draw(screen)
+        
